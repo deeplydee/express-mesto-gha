6508d4f8e2e106696b3aad427aa2923cc32d3b2b
deeplydee <dmitry.pletyukhin@gmail.com>
2022-09-18 13:12:06 +0300

    add validation

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+const { errors, celebrate, Joi } = require('celebrate');
 
 const auth = require('./middlewares/auth');
 const { createUser, login } = require('./controllers/users');
@@ -27,9 +28,29 @@ main();
 app.use(express.json());
 app.use(cookieParser());
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post(
+  '/signin',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required().min(6).trim(),
+    }),
+  }),
+  login,
+);
+
+app.post(
+  '/signup',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required().min(6).trim(),
+    }),
+  }),
+  createUser,
+);
 
 app.use(auth);
 
 app.use(routes);
+app.use(errors());
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { celebrate, Joi } = require('celebrate');
 
 const userRoutes = express.Router();
 const {
@@ -9,10 +10,43 @@ const {
   getUserInfo,
 } = require('../controllers/users');
 
-userRoutes.get('/me', getUserInfo);
-userRoutes.get('/:id', getUserById);
 userRoutes.get('/', getUsers);
-userRoutes.patch('/me', updateUserProfile);
-userRoutes.patch('/me/avatar', updateUserAvatar);
+
+userRoutes.get('/me', getUserInfo);
+
+userRoutes.get(
+  '/:id',
+  celebrate({
+    params: Joi.object().keys({
+      id: Joi.string().required().length(24).hex(),
+    }),
+  }),
+  getUserById,
+);
+
+userRoutes.patch(
+  '/me',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required().min(2).max(30),
+      about: Joi.string().required().min(2).max(30),
+    }),
+  }),
+  updateUserProfile,
+);
+
+userRoutes.patch(
+  '/me/avatar',
+  celebrate({
+    body: Joi.object().keys({
+      avatar: Joi.string()
+        .required()
+        .pattern(
+          /^(https?:\/\/)?([a-zA-z0-9%$&=?/.-]+)\.([a-zA-z0-9%$&=?/.-]+)([a-zA-z0-9%$&=?/.-]+)?(#)?$/,
+        ),
+    }),
+  }),
+  updateUserAvatar,
+);
 
 module.exports = { userRoutes };
