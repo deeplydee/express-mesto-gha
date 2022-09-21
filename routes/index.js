0cd56a35c250b1b06b6cd1fd8756c90aee324585
deeplydee <dmitry.pletyukhin@gmail.com>
2022-09-21 15:02:46 +0300

    fix after autotests

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,13 @@ const routes = express.Router();
 const { userRoutes } = require('./users');
 const { cardRoutes } = require('./cards');
 
-const { NotFoundError } = require('../helpers/errors/not-found-error');
+const { NOT_FOUND_CODE } = require('../helpers/constants');
 
 routes.use('/users', userRoutes);
 routes.use('/cards', cardRoutes);
 
-routes.use('*', (req, res, next) => {
-  next(new NotFoundError('Не найдено'));
+routes.use('/*', (req, res) => {
+  res.status(NOT_FOUND_CODE).send({ message: 'Не найдено' });
 });
 
 module.exports = { routes };
